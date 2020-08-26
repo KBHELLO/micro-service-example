bd8201a004ceef8c798992c8aa4d185d093d2d68
KBHELLO <banerjee4785@gmail.com>
2020-08-26 13:01:18 -0700

    about added in controller.js

--- api/controller.js
+++ api/controller.js
@@ -4,5 +4,11 @@ const properties = require('../package.json');
 const distance = require('../service/distance');
 
 const controller = {
-       
+    about: function(req, res) {
+        const aboutInfo = {
+            name: properties.name,
+            version: properties.version
+        };
+        res.json(aboutInfo);
+    }
 };
